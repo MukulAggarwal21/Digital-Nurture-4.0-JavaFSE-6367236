ee9f3ecb227d0aa6c44c6a49b552b220316185ab
MukulAggarwal21 <mukul.2226csit1115@kiet.edu>
2025-07-18 22:47:17 +0530

    Additional HandsON Week 5

---
+++ Week 5/Additional HandsON/Eureka Discovery Server and register/greet-service/src/main/java/com/cognizant/greetservice/GreetServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.greetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableDiscoveryClient
+
+@SpringBootApplication
+public class GreetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetServiceApplication.class, args);
+    }
+}
